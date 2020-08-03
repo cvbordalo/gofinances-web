72d9837ead568a26ac5332f09c250a5fa37778ec
Carlos Bordalo <carlosvitor6@hotmail.com>
2020-08-03 15:51:44 -0300

    feat: Add navigation to import page

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,8 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+/* const formatValue = (value: number): string =>
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value); // TODO
 
 export default formatValue;
+ */
